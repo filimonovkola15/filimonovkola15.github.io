3fbb617a2e6aea6e0db4f46a904b9eca2d36a71b
filimonovkola15 <68072262+filimonovkola15@users.noreply.github.com>
2025-08-31 12:37:05 +0300

    вв

---
+++ archive/forms-insert/index.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <!-- Обязательные метатеги -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style/stule.css">
+    <title>Привет мир!</title>
+  </head>
+  <body>
+<!-- Select -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-css/1.4.6/select2-bootstrap.min.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.4/css/select2.min.css" rel="stylesheet" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.4/js/select2.min.js"></script>
+
+
+<!-- Styles --><div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Create a new release</h1>
+           <ol class="breadcrumb mb-4"></ol>
+
+            <form method="POST" action="" enctype="multipart/form-data">
+    
+                <div class="row" bis_skin_checked="1">
+                    <body onload="shows_form_part(1)">
+                    <form>
+                        <div id="form_part1">
+                            <nav>
+                                <div class="row" bis_skin_checked="1">
+                                    <div class="col" bis_skin_checked="1">
+                                        <div class="card" bis_skin_checked="1">
+                                            <div class="card-header" bis_skin_checked="1">                                             
+                                                <b>Release Main Info</b>
+                                            </div>
+
+                                            <div class="card-body" bis_skin_checked="1">
+                                                <div class="form-floating mb-3 ms-2 me-2" bis_skin_checked="1">
+                                                    <input type="text" class="form-control" name="releaseName" id="releaseName" required="" placeholder="Release name">
+                                                    <label for="releaseName">Release name*</label>
+                                                </div>
+                                                <div class="form-floating mb-3 ms-2 me-2" bis_skin_checked="1">
+                                                    <input type="text" class="form-control" name="Mixversion"   placeholder="Mixversion">
+                                                    <label for="releaseName">Mixversion</label>
+                                                </div>
+                                                <div class="form-floating mb-3 ms-2 me-2" bis_skin_checked="1">
+                                                    <input type="text" class="form-control" name="ReleaseCatalogNo" id="releaseName" required="" placeholder="Release Catalog No">
+                                                    <label for="releaseName">Release Catalog No</label>
+                                                </div>
+                                                <div class="form-floating ms-2 me-2 mb-3">
+                                                    <select class="form-select" name="Releasemaingenre" id="Releasemaingenre" aria-label="Release main genre">
+                                                        <option value="Hip Hop/Rap">Hip Hop/Rap</option>
+                                                        <option value="Audiobooks">Audiobooks</option>
+                                                        <option value="Blues">Blues</option>
+                                                        <option value="Children's Music">Children's Music</option>
+                                                        <option value="Classical">Classical</option>
+                                                        <option value="Comedy">Comedy</option>
+                                                        <option value="Country">Country</option>
+                                                        <option value="Dance">Dance</option>
+                                                        <option value="Electronic">Electronic</option>
+                                                        <option value="Folk">Folk</option>
+                                                        <option value="Holiday">Holiday</option>
+                                                        <option value="Inspirational">Inspirational</option>
+                                                        <option value="Jazz">Jazz</option>
+                                                        <option value="Latin">Latin</option>
+                                                        <option value="New Age">New Age</option>
+                                                        <option value="Opera">Opera</option>
+                                                        <option value="Pop">Pop</option>
+                                                        <option value="Rock">Rock</option>
+                                                        <option value="R&B/Soul">R&amp;B/Soul</option>
+                                                        <option value="Reggae">Reggae</option>
+                                                        <option value="Soundtrack">Soundtrack</option>
+                                                        <option value="Spoken Word">Spoken Word</option>
+                                                        <option value="Vocal">Vocal</option>
+                                                        <option value="Alternative">Alternative</option>
+                                                        <option value="World">World</option>
+                                                    </select>
+                                                    <label for="releaseGenre">Release main genre*</label>
+                                                </div>
+                                                <div class="form-floating ms-2 me-2 mb-3">
+                                                    <b class="mt-2">Release Type:*</b>
+                                                    <br>
+                                                    <div class="btn-group mt-2"  role="group" aria-label="releaseType" bis_skin_checked="1">
+                                                        <input type="radio" class="btn-check" value="Single"  name="releaseTypeBtn" id="releaseTypeSingle" autocomplete="off" checked="">
+                                                        <label class="btn btn-outline-dark" for="releaseTypeSingle">Single</label>
+
+                                                        <input type="radio" class="btn-check"  value="EP" name="releaseTypeBtn" id="releaseTypeEP" autocomplete="off">
+                                                        <label class="btn btn-outline-dark" for="releaseTypeEP">EP</label>
+
+                                                        <input type="radio" class="btn-check" value="Album" name="releaseTypeBtn" id="releaseTypeAlbum" autocomplete="off">
+                                                        <label class="btn btn-outline-dark" for="releaseTypeAlbum">Album</label>
+                                                    </div><br>
+                                                </div>
+                                                <div class="form-floating mt-3 mb-3 ms-2 me-2" bis_skin_checked="1">
+                                                    <input data-provide="datepicker-inline" type="date" name="date" autocomplete="off" data-date-format="yyyy-mm-dd" class="form-control" id="releaseDate" required="" placeholder="Release Date">
+                                                    <label for="releaseDate">Release Date*</label>
+                                                </div>
+                                                <div class="form-floating mt-3 mb-3 ms-2 me-2" bis_skin_checked="1">
+                                                    <input data-provide="datepicker-inline" type="date" name="preorder" autocomplete="off" data-date-format="yyyy-mm-dd" class="form-control" id="releaseDate" required="" placeholder="Release Date">
+                                                    <label for="releaseDate">Preorder Date*</label>
+                                                </div>
+
+                                                <div class="form-floating mt-3 mb-3 ms-2 me-2" bis_skin_checked="1">
+
+                                                <div class="btn-group mt-2"  role="group" >
+                                                    <input type="radio" class="btn-check" name="Explicit" id="success-outlined" autocomplete="off" checked value="Yes" bis_skin_checked="1">
+                                                    <label class="btn btn-outline-dark" for="success-outlined">Yes</label>
+
+                                                    <input type="radio" class="btn-check" name="Explicit" id="danger-outlined" autocomplete="off" value="No" bis_skin_checked="1">
+                                                    <label class="btn btn-outline-dark" for="danger-outlined">No</label>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col mb-4" bis_skin_checked="1">
+                                        
+                                        <div class="card" bis_skin_checked="1">
+                                            <div class="card-header" bis_skin_checked="1">
+                                            
+                                                <b>Artists</b>
+                                            </div>
+                                            <div class="card-body" bis_skin_checked="1">
+                                                <table class="table">
+                                                    <tbody>
+
+                                                    </div>
+                                                    <tr>
+                                                        <div class="input-group">
+                                                            <td>
+                                                                <input class="form-control" name="releaseArtist[]" id="releaseArtist" placeholder="ReleaseArtist">
+                                                            </td>
+                                                            <td><a href="javascript:;" class="btn btn-dark addrow" role="button">Create</a></td>
+                                                        </div>
+                                                    </tr>
+                                                    <tr>
+                                                        <div class="input-group">
+                                                            <td>
+                                                                 <input class="form-control" name="featuring[]" id="FeatArtist" placeholder="FeatArtist">
+                                                            </td>
+                                                    <td><a href="javascript:;" class="btn btn-dark addfeat" role="button">Create</a></td>
+                                                        </div>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+
+                                        <ol class="breadcrumb mb-4"></ol>
+
+                                        <div class="card" bis_skin_checked="1">
+                                            <div class="card-header" bis_skin_checked="1">
+                                                
+                                                <b>Release Copyright Information</b>
+                                            </div>
+                                            <div class="card-body" bis_skin_checked="1">
+                                                <div class="form-floating mt-3 mb-3 ms-2 me-2" bis_skin_checked="1">
+                                                    <input data-provide="datepicker-inline" type="date" name="text" autocomplete="off" data-date-format="yyyy-mm-dd" class="form-control" id="releaseDate" required="" placeholder="Release Date">
+                                                    <label for="releaseDate">Release Copyright Year (©)</label>
+                                                </div>
+                                                <div class="form-floating mt-3 mb-3 ms-2 me-2" bis_skin_checked="1">
+                                                    <input data-provide="datepicker-inline" type="text" name="date" autocomplete="off" data-date-format="yyyy-mm-dd" class="form-control" id="releaseDate" required="" placeholder="Release Date">
+                                                    <label for="releaseDate">Release Copyright Text (©)</label>
+                                                </div>
+                                                <div class="form-floating mt-3 mb-3 ms-2 me-2" bis_skin_checked="1">
+                                                    <input data-provide="datepicker-inline" type="date" name="date" autocomplete="off" data-date-format="yyyy-mm-dd" class="form-control" id="releaseDate" required="" placeholder="Release Date">
+                                                    <label for="releaseDate">Release Phonogram Year (℗)</label>
+                                                </div>
+                                                <div class="form-floating mt-3 mb-3 ms-2 me-2" bis_skin_checked="1">
+                                                    <input data-provide="datepicker-inline" type="text" name="date" autocomplete="off" data-date-format="yyyy-mm-dd" class="form-control" id="releaseDate" required="" placeholder="Release Date">
+                                                    <label for="releaseDate">Release Phonogram Text (℗)</label>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+
+
+                            </nav>                                                  
+                        </div>                            
+                </div>
+                <br>
+            </form>
+        </div>
+    </main>
+</div>
+</div>
+</body>
+</html>
+
+<script>
+    $('tbody').on('click', '.addrow',function (){
+        var tr = '<tr>'+
+            '<td>'+
+            '<input class="form-control" name="releaseArtist[]" id="releaseArtist" placeholder="ReleaseArtist">'+
+            '</td>'+
+        '<td><a href="javascript:;"  class="btn btn-danger deleterow" role="button">Delete</a></td>'+
+    '</tr>';
+        $ ('tbody').append(tr);
+    });
+    $('tbody').on('click', '.deleterow',function (){
+        $(this).parent().parent().remove();
+    })
+
+    $('tbody').on('click', '.addfeat',function (){
+        var tr = '<tr>'+
+            '<td>'+
+            '<input class="form-control" name="featuring[]" id="featuring" placeholder="FeatArtist">'+
+            '</td>'+
+            '<td><a href="javascript:;"  class="btn btn-danger deleterow" role="button">Delete</a></td>'+
+            '</tr>';
+        $ ('tbody').append(tr);
+    });
+    $('tbody').on('click', '.deletefeat',function (){
+        $(this).parent().parent().remove();
+    })
+    $(".js-select2").select2({
+        closeOnSelect : false,
+        placeholder : "add a track",
+        // allowHtml: true,
+        allowClear: true,
+        tags: true // создает новые опции на лету
+    });
+</script>
